cec9a121fba81bdfea6bfa5fd7f2d22a18336cff
Josh Mann <jlmann72@gmail.com>
2020-08-18 03:44:30 -0400

    Created methods to replace repeated code

--- Program.cs
+++ Program.cs
@@ -2,11 +2,43 @@
 
 namespace EmployeeDatabase
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      Console.WriteLine("Welcome to C#");
+
+        static void DisplayGreeting()
+        {
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("    Welcome to Our Employee Database    ");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine();
+
+        }
+
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = int.Parse(Console.ReadLine());
+            return userInput;
+        }
+        static void Main(string[] args)
+        {
+            DisplayGreeting();
+
+            var name = PromptForString("What is your name? ");
+
+            int department = PromptForInteger("What is your department number? ");
+
+            int salary = PromptForInteger("What is your yearly salary (in dollars)? ");
+
+            Console.WriteLine($"Hello, {name} you make {salary / 12} dollars per month.");
+        }
     }
-  }
 }
